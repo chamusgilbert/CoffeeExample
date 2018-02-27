3d1437de77e79ab76fa4045d92e4d582bcdd3301
famouschamus <chamusg@gmail.com>
2018-02-27 11:03:15 -0500

    initial commit

---
+++ CoffeeExample/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using Lab20.Models;
+using System.Web.Mvc;
+
+namespace Lab20.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(Register r)
+        {
+
+            ViewBag.Message = "Welcome to the Grand Circus Coffee App";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contact us.";
+
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            ViewBag.Message = "Register with Grand Circus Coffee!";
+            return View();
+        }
+
+        public ActionResult Success(Register r)
+        {
+
+            ViewBag.Message = "Welcome " + r.FirstName;
+            Session["Email"] = r.Email;
+            Session["Name"] = r.FirstName + " " + r.LastName;
+            Session["Phone"] = r.Phone;
+            return View(r);
+        }        //public ActionResult Cookie()
+        //{
+        //    HttpCookie myCookie = new HttpCookie("User");
+        //    myCookie["Name"] = "Sebastian";
+        //    myCookie["Color"] = "Blue";
+        //    myCookie.Expires{ 1; 2; };
+        //    Response.Cookies.Add(myCookie);
+        //    return View();
+
+        //    //if (Request.Cookies["User"] != null)
+        //}
+
+    }
+}
---
+++ CoffeeExample/Models/Register.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab20.Models
+{
+    public class Register
+    {
+        [Required]
+        [DisplayFormat(ConvertEmptyStringToNull = false)]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "Enter A Valid First Name")]
+        [Key]
+        public string FirstName { get; set; }
+
+        [Required]
+        [DisplayFormat(ConvertEmptyStringToNull = false)]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "Enter A Valid First Name")]
+        [Key]
+        public string LastName { get; set; }
+
+        [Required]
+        [DisplayFormat(ConvertEmptyStringToNull = false)]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "Enter A Valid Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(3)]
+        public string Password { get; set; }
+
+
+        public string Phone { get; set; }
+
+        [Required]
+        public bool EmailMe { get; set; }
+
+
+    }
+
+}
